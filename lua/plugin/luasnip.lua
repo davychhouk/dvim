b79ce18365891993125bfd9d7384788910aa544f
Davy Chhouk <chhouk.davy@gmail.com>
2026-05-26 01:44:11 -0500

    feat:configure snippets setup

--- lua/plugin/luasnip.lua
+++ lua/plugin/luasnip.lua
@@ -5,9 +5,22 @@ return {
 	build = "make install_jsregexp",
 	event = "VeryLazy",
 	config = function()
-		-- Required for friendly-snippets
+		local ls = require("luasnip")
+
+		ls.setup({
+			history = true,
+			update_events = "TextChanged,TextChangedI",
+			enable_autosnippets = true,
+			store_selection_keys = "<Tab>",
+		})
+
+		ls.filetype_extend("javascriptreact", { "javascript" })
+		ls.filetype_extend("typescript", { "javascript" })
+		ls.filetype_extend("typescriptreact", { "typescript" })
+
+		-- Load friendly-snippets (VSCode format, ships with the plugin)
 		require("luasnip.loaders.from_vscode").lazy_load()
-		-- Load custom snippets
-		require("luasnip.loaders.from_vscode").lazy_load({ paths = vim.fn.stdpath("config") .. "/snippets" })
+		-- Load custom Lua-format snippets
+		require("luasnip.loaders.from_lua").lazy_load({ paths = vim.fn.stdpath("config") .. "/snippets" })
 	end,
 }
