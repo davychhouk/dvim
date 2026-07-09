0c25ac7cca0c2b08af348b54a8d1448c348116ed
Davy Chhouk <chhouk.davy@gmail.com>
2026-07-09 13:41:26 -0500

    chore: update rustaceanvim setup

--- lua/plugin/rustaceanvim.lua
+++ lua/plugin/rustaceanvim.lua
@@ -1,17 +1,5 @@
 return {
   "mrcjkb/rustaceanvim",
-  version = "^6",
-  ft = { "rust" },
-  config = function()
-    local cfg = require("rustaceanvim.config")
-    local codelldb_ext_path = vim.fn.expand("$MASON/packages/codelldb") .. "/extension"
-    local codelldb_path = codelldb_ext_path .. "/adapter/codelldb"
-    local liblldb_ext = vim.uv.os_uname().sysname == "Darwin" and ".dylib" or ".so"
-    local liblldb_path = codelldb_ext_path .. "/lldb/lib/liblldb" .. liblldb_ext
-    vim.g.rustaceanvim = {
-      dap = {
-        adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
-      },
-    }
-  end,
+  version = "^9",
+  lazy = false,
 }
