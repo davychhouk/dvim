5a89c0197238778efc59ba65fc3b9eea902bffca
Davy Chhouk <chhouk.davy@gmail.com>
2026-07-12 16:37:00 -0500

    fix: solved rust-analyzer root scanned 100% stuck

--- lua/plugin/rustaceanvim.lua
+++ lua/plugin/rustaceanvim.lua
@@ -2,4 +2,15 @@ return {
   "mrcjkb/rustaceanvim",
   version = "^9",
   lazy = false,
+  init = function()
+    vim.g.rustaceanvim = {
+      server = {
+        default_settings = {
+          ["rust-analyzer"] = {
+            files = { excludeDirs = { ".direnv" } },
+          },
+        },
+      },
+    }
+  end,
 }
